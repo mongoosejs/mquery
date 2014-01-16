5c75bcf06f66aa51068b3afa1294e71ec4b06065
Aaron Heckmann <aaron@recurly.com>
2014-01-16 08:24:35 -0800

    added; better ObjectId clone support

--- test/utils.test.js
+++ test/utils.test.js
@@ -16,6 +16,42 @@ describe('lib/utils', function() {
       done();
     });
 
+    it('clones constructors named ObjectID', function(done) {
+      function ObjectID (id) {
+        this.id = id;
+      }
+
+      var o1 = new ObjectID('1234');
+      var o2 = utils.clone(o1);
+
+      assert.ok(o2 instanceof ObjectID);
+      done();
+    });
+
+    it('optionally clones ObjectId constructors using its clone method', function(done) {
+      function ObjectID (id) {
+        this.id = id;
+        this.cloned = false;
+      }
+
+      ObjectID.prototype.clone = function () {
+        var ret = new ObjectID(this.id);
+        ret.cloned = true;
+        return ret;
+      }
+
+      var id = 1234;
+      var o1 = new ObjectID(id);
+      assert.equal(id, o1.id);
+      assert.equal(false, o1.cloned);
+
+      var o2 = utils.clone(o1);
+      assert.ok(o2 instanceof ObjectID);
+      assert.equal(id, o2.id);
+      assert.ok(o2.cloned);
+      done();
+    });
+
     it('handles objects with no constructor', function(done) {
       var name ='335';
 
